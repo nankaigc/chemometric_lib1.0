ac47a09438ea271258879b697bc0076b627068f6
nankaigc <34182808+nankaigc@users.noreply.github.com>
2020-06-17 11:19:49 +0800

    1

--- lib_gc/draw_for_paper.m
+++ lib_gc/draw_for_paper.m
@@ -3,8 +3,8 @@
 set(gcf,'Position',[100 100 360 300]);
 
 figure_FontSize=13;
-set(get(gca,'XLabel'),'FontSize',figure_FontSize,'Vertical','top','FontName','Times New Roman');
-set(get(gca,'YLabel'),'FontSize',figure_FontSize,'Vertical','middle','FontName','Times New Roman');
+% set(get(gca,'XLabel'),'FontSize',figure_FontSize,'Vertical','top','FontName','Times New Roman');
+% set(get(gca,'YLabel'),'FontSize',figure_FontSize,'Vertical','middle','FontName','Times New Roman');
 set(gca,'FontSize',figure_FontSize,'FontName','Times New Roman');
 set(findobj('FontSize',figure_FontSize),'FontSize',figure_FontSize,'FontName','Times New Roman');
 
