44dcf4e975cf6b554c5c45bbd517076a5cd97644
nankaigc <34182808+nankaigc@users.noreply.github.com>
2020-07-13 16:09:17 +0800

    1

--- lib_gc/draw_for_paper.m
+++ lib_gc/draw_for_paper.m
@@ -1,11 +1,11 @@
 %fig_Y_pred(cal_y,cvpredpls(:,:,LVs),'C')
 % set(gcf,'Position',[100 100 340 300]);
-set(gcf,'Position',[100 100 360 300]);
+set(gcf,'Position',[100 100 240 200]);
 
 figure_FontSize=13;
 % set(get(gca,'XLabel'),'FontSize',figure_FontSize,'Vertical','top','FontName','Times New Roman');
 % set(get(gca,'YLabel'),'FontSize',figure_FontSize,'Vertical','middle','FontName','Times New Roman');
-set(gca,'FontSize',figure_FontSize,'FontName','Times New Roman');
-set(findobj('FontSize',figure_FontSize),'FontSize',figure_FontSize,'FontName','Times New Roman');
+set(gca,'FontSize',figure_FontSize,'FontName','Arial');
+set(findobj('FontSize',figure_FontSize),'FontSize',figure_FontSize,'FontName','Helvetica');
 
 clear figure_FontSize
